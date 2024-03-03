cf44470e89e3fe54d3f6f489871fbe251aca8fba
pbrdys <142018441+pbrdys@users.noreply.github.com>
2024-03-03 19:43:17 +0100

    removed field_1 from Post-Model

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,6 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    field_1 = models.CharField()
     
     def __str__(self):
         return f"{self.title} | written by {self.author}"
