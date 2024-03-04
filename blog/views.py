ad2245028b66c1832a374ac28fe7d01107579167
pbrdys <142018441+pbrdys@users.noreply.github.com>
2024-03-04 15:30:25 +0100

    Working on View Filters

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,9 @@ from .models import Post
 # Create your views here.
 # def index(request):
 #     return HttpResponse("Ello, ello, Blog og og og og")
+
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    # queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
+    # queryset = Post.objects.all().order_by("created_on")
     template_name = "post_list.html"
