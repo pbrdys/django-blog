b85bc52fe75281af6cfef277010cd5a71c4046f6
pbrdys <142018441+pbrdys@users.noreply.github.com>
2024-03-14 16:40:53 +0100

    Handle POST form request (save to DB)

--- about/views.py
+++ about/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
-from django.views import generic
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
 # Create your views here.
 def about_me(request):
     about = About.objects.all().order_by('-updated_on').first()
+    
+    if(request.method == "POST"):
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+            
     collaborate_form = CollaborateForm()
     
     return render(
