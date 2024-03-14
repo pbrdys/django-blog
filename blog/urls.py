a297dd116f4ce5d3644f60369506dddb885473ea
pbrdys <142018441+pbrdys@users.noreply.github.com>
2024-03-14 22:49:20 +0100

    Edit comment

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'), #class based view
     path('<slug:slug>/', views.post_detail, name='post_detail'), # function based view
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
 ]
--- static/js/script.js
+++
@@ -1,1 +0,0 @@
-console.log("a message to the console!")
