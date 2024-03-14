66fbab2b26929442ad3f636a01f1ae6d6225405d
pbrdys <142018441+pbrdys@users.noreply.github.com>
2024-03-14 12:53:46 +0100

    Create CollaborationRequest Form

--- about/views.py
+++ about/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
 from django.views import generic
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 def about_me(request):
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
     
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form, 
+        },
     )
